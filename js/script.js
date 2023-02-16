f7e9148c8173967d066ace78f00b6b80607f3d1e
Hugo <006.tran.h@gmail.com>
2023-02-15 23:10:38 -0800

    Initial

---
+++ js/script.js
@@ -0,0 +1,118 @@
+//STEP 1
+// let someMonth;
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+//STEP 2
+// 2.15 - Numeric literal experession
+// 'Hello' - String literal expression
+// true - Boolean literal expression
+// null - Null literal expression
+
+//STEP 3
+// let firstExpression =  2 * (2/6) -2;
+// let secondExpression = 4 / (3/5) + 1;
+
+//STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let zipCode
+// let yourAge
+// let referralSource
+// let booleanMayWeContactYou
+
+//STEP 5
+// let firstName = "Hugo";
+// let lastName = "Tran";
+// let address = "1234 Place";
+
+//STEP 6
+// let exampleString = "Hello" + 3;
+// console.log(exampleString);
+
+//STEP 7
+// let exampleBoolean1 = true + "word";
+// console.log(exampleBoolean1);
+// let exampleBoolean2 = true + 2;
+// console.log(exampleBoolean2);
+
+//STEP 8
+// let someString =
+//   'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+//STEP 9
+// let nullValue = null;
+// console.log(nullValue);
+
+//STEP 10
+// let undefinedVariable;
+// console.log(undefinedVariable);
+
+//STEP 11
+// alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");
+
+//STEP 12
+// let myName = "Hugo Tran";
+// alert("Hello " + myName + ", welcome to the JavaScript class!");
+
+//STEP 13
+// let myName = "Hugo Tran";
+// let thisCourse = "JavaScript";
+// alert("Hello " + myName + ", welcome to the " + thisCourse + " class!");
+
+//STEP 14
+// let myName = "Hugo Tran";
+// let thisCourse = "JavaScript";
+// alert("Hello " + myName + ". \n" + "Welcome to the " + thisCourse + " class!");
+
+//STEP 15
+// let thisCourse = "JavaScript";
+// let promptInput = prompt("What is your name?");
+// alert(
+//   "Hello " + promptInput + ". \n" + "Welcome to the " + thisCourse + " class!"
+// );
+
+//STEP 16
+// let promptName = prompt("What is your name?");
+// let promptCourse = prompt("What course are you taking?");
+// alert(
+//   "Hello " + promptName + ". \n" + "Welcome to the " + promptCourse + " class!"
+// );
+
+//STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+//STEP 18
+// let x1 = 20;
+// x1 += 20;
+// console.log(x1);
+
+//STEP 19
+// let x2 = 20;
+// x2 *= 5;
+// console.log(x2);
+
+//STEP 20
+// let x3 = 20 % 3;
+// x3 /= 1;
+// console.log(x3);
+
+//STEP 21
+// let first = 1;
+// let second = 2;
+
+// console.log(first > 0 && second > 0);
+
+//STEP 22
+// let third = 1;
+// let fourth = 2;
+
+// console.log(third < 0 || fourth < 0);
